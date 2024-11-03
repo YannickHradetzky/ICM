1c02544aba7a3de0b7dddf1e9ecc5b2bb5b5c6cf
YannickHradezky <hradetzky.y@gmail.com>
2024-11-03 14:32:19 +0000

    added script for monthly mean

---
+++ analysis/monthly_mean_gif.py
@@ -0,0 +1,59 @@
+import xarray as xr
+import numpy as np
+import time
+from dask.distributed import Client, LocalCluster
+import matplotlib.pyplot as plt
+from glob import glob 
+import my_functions as mf
+import argparse
+
+
+def main(year, client):
+    path="/home/yannickh00/LEHRE/msc-intro-comp-met-ex-w2024/data/era5/"    
+    print
+    # open the dataset
+    ds=xr.open_mfdataset(path+f"era5-{year}-*.nc", engine="netcdf4", chunks={"valid_time":1e5} )
+    
+    # calculate the windsped
+    ds["wspd"] = mf.calc_ws(ds)
+    # calculate the pv potential and group it by months
+    print("Calculating pv mean")
+    pvpot = mf.calc_pv_pot(ds).groupby(ds.valid_time.dt.month).mean("valid_time").compute()
+
+    # shutdown the client
+    client.shutdown()
+    return pvpot
+
+def make_figure(pvpot, month, year):
+    image_path = "/home/yannickh00/ICM/analysis/monthly_averages/"
+    plt.contourf(pvpot.longitude, pvpot.latitude, pvpot.sel(month=month))
+    plt.colorbar()
+    plt.savefig(image_path + f"{month}-{year}.png")
+    plt.close()
+
+if __name__ == "__main__":
+    start = time.time()
+    # Set up argument parsing
+    parser = argparse.ArgumentParser(description="Process ERA5 data.")
+    parser.add_argument("year", type=str, help="year to compute the mean for")
+    
+    # Parse the command-line arguments
+    args = parser.parse_args()
+
+    # Create a client
+    client = Client(n_workers=20, threads_per_worker=5, local_directory='~/tmp')
+
+    # Call the main function with the provided path
+    mean_data = main(args.year, client)
+
+    # make a figure
+    print("Making the figures")
+    for month in range(1,13):
+        make_figure(mean_data, month, args.year)
+
+    end = time.time()
+    print(f"Program took {end-start:.4f} seconds")
+
+
+    
+    
---
+++ analysis/my_functions.py
@@ -0,0 +1,29 @@
+import numpy as np
+import xarray as xr
+import time
+
+
+def calc_ws(ds):
+    return np.sqrt(np.power(ds["u10"], 2)+np.power(ds["v10"], 2))
+
+
+def calc_pv_pot(ds):
+    c1 = 4.3
+    c2 = 0.943
+    c3 = 0.028
+    c4 = -1.528
+    beta = -0.005
+
+    T_cell = c1 + c2 * (ds.t2m - 273.15) + c3 * ds.ssrd/3600 + c4 * ds.wspd
+
+    perf = 1 + beta*(T_cell-25)
+
+    pv_pot = perf * ds.ssrd/3600 * 1/100
+    return pv_pot
+
+
+def calculate_mean_year(year:str, path = "/home/yannickh00/LEHRE/msc-intro-comp-met-ex-w2024/data/era5/"):
+    ds=xr.open_mfdataset(path+"era5-{year}-*.nc", engine="netcdf4", chunks={"valid_time":1e5} )
+    ds["wspd"] = calc_ws(ds)
+    pvpot = calc_pv_pot(ds).groupby(ds.valid_time.dt.month).mean("valid_time").compute()
+    return pvpot, ds
